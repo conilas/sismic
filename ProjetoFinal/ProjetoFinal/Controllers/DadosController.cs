1ddb03a867dc38bc81b7a004f2391f725bcf0de1
fmachado34 <felipe.duarte@innovix.com.br>
2017-11-29 21:47:35 -0200

    animal

---
+++ ProjetoFinal/ProjetoFinal/Controllers/DadosController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProjetoFinal.Models;
+
+namespace ProjetoFinal.Controllers
+{
+    public class DadosController : ApiController
+    {
+        public static string relay_state = "";
+        private string relay_change = "";
+
+        // GET: api/Dados
+        public string Get()
+        {
+            return relay_state;
+        }
+
+        // GET: api/Dados/5
+        public string Get(int id)
+        {
+            return relay_state;
+        }
+
+        [HttpPost]
+        // POST: api/Dados
+        public HttpResponseMessage Post([FromBody]Relays value)
+        {
+            if (value != null)
+            {
+                relay_state = value.relay;
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
+            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+        }
+
+        // PUT: api/Dados/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Dados/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ProjetoFinal/ProjetoFinal/Models/Relays.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoFinal.Models
+{
+    public class Relays
+    {
+        public string relay { get; set; }
+    }
+}
